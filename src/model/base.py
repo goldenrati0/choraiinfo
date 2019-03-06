b0ed7fcbe80e8203897885247ba71dcd78e3ce0a
tahmid-choyon <tahmid.choyon@gmail.com>
2019-03-06 20:26:09 +0600

    Remove metaclass from Abstract Base Class, Optimize BaseModel methods
    - Remove MetaBaseModel
    - Optimize __repr__ and json method to ignore column that starts with '_' (underscore)

--- src/model/base.py
+++ src/model/base.py
@@ -3,37 +3,15 @@ Define an Abstract Base Class (ABC) for models
 """
 from datetime import datetime
 from typing import Union, Tuple
-from weakref import WeakValueDictionary
 
 from sqlalchemy import inspect
-from sqlalchemy.orm import aliased
 
 from src.setup import db
-
-
-class MetaBaseModel(db.Model.__class__):
-    """ Define a metaclass for the BaseModel
-        Implement `__getitem__` for managing aliases """
-
-    def __init__(cls, *args):
-        super().__init__(*args)
-        cls.aliases = WeakValueDictionary()
-
-    def __getitem__(cls, key):
-        try:
-            alias = cls.aliases[key]
-        except KeyError:
-            alias = aliased(cls)
-            cls.aliases[key] = alias
-        return alias
 
 
 class BaseModel():
     """ Generalize __init__, __repr__ and to_json
         Based on the models columns """
-
-    print_filter = ()
-    to_json_filter = ()
 
     def __repr__(self):
         """ Define a base way to print models
@@ -41,7 +19,7 @@ class BaseModel():
         return '%s(%s)' % (self.__class__.__name__, {
             column: value
             for column, value in self._to_dict().items()
-            if column not in self.print_filter
+            if not column.startswith("_")
         })
 
     @property
@@ -52,7 +30,7 @@ class BaseModel():
             column: value
             if not isinstance(value, datetime) else value.strftime('%Y-%m-%d')
             for column, value in self._to_dict().items()
-            if column not in self.to_json_filter and not column.startswith("_")
+            if not column.startswith("_")
         }
 
     def _to_dict(self):
