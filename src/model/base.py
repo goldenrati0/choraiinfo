2ef7db5cecdd8589b612f9193b3fb0e69864af63
tahmid-choyon <tahmid.choyon@gmail.com>
2019-03-11 10:12:27 +0600

    Fix Base model's function doc

--- src/model/base.py
+++ src/model/base.py
@@ -15,7 +15,7 @@ class BaseModel():
 
     def __repr__(self):
         """ Define a base way to print models
-            Columns inside `print_filter` are excluded """
+            Columns that start with _ are excluded """
         return '%s(%s)' % (self.__class__.__name__, {
             column: value
             for column, value in self._to_dict().items()
@@ -25,7 +25,7 @@ class BaseModel():
     @property
     def json(self):
         """ Define a base way to jsonify models
-            Columns inside `to_json_filter` are excluded """
+            Columns that start with _ are excluded """
         return {
             column: value
             if not isinstance(value, datetime) else value.strftime('%Y-%m-%d')
