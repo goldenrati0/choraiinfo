c5019ca2623d9df5d547b4ae3611b4bc40d607ee
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-04-07 04:59:14 +0600

    Add Flask Admin

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,6 +1,21 @@
+from flask_admin import Admin
+
 from src.controller.blueprints.lost_item import lostitem_blueprint
 from src.controller.blueprints.user import user_blueprint
-from src.setup import app
+from src.setup import app, db
+from flask_admin.contrib.sqla import ModelView
+
+from src.model import User, Vehicle, Laptop, CellPhone, SearchHistory
+
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='choraiinfo', template_mode='bootstrap3')
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Vehicle, db.session))
+admin.add_view(ModelView(Laptop, db.session))
+admin.add_view(ModelView(CellPhone, db.session))
+admin.add_view(ModelView(SearchHistory, db.session))
 
 app.register_blueprint(user_blueprint, url_prefix="/user")
 app.register_blueprint(lostitem_blueprint, url_prefix="/item")
